cad4a69a72818ebae7485dc77597fa1b2a10eea2
MadMolecularMan <omnitrophic@gmail.com>
2020-04-07 14:42:52 -0400

    Added more lines of explanation and edited line 3 not 2

--- data_analysis.sh
+++ data_analysis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #This will contain my new analytical method!
-#Typing for the sake of typing
+#Typing for typing
 #We are using this file to aid in using git for version control
+#There will be many instances where this is helpful
+#We may be adding new lines of code
+#We might alter other things as well, such as deleting parts of line 
