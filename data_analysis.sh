6b362dd1cd58217764c8e4a7395132ce3fc342dc
MadMolecularMan <omnitrophic@gmail.com>
2020-04-07 15:04:31 -0400

    added functions

--- data_analysis.sh
+++ data_analysis.sh
@@ -4,5 +4,14 @@
 #We are using this file to aid in using git for version control
 #There will be many instances where this is helpful
 #We may be adding new lines of code
-#We might alter other things as well, such as deleting parts of line 
+#We might alter other things as well, such as deleting parts of line
+
 #Lets make a change here to work with git diff
+
+#Function 1
+
+#Function 2
+
+#Function 3
+
+#Function 4
