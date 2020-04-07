cfaaf6a4aaa6e79a5cc1a83b592a7a5ec36ceaac
MadMolecularMan <omnitrophic@gmail.com>
2020-04-07 14:52:21 -0400

    add a line to mess with git diff

--- data_analysis.sh
+++ data_analysis.sh
@@ -5,3 +5,4 @@
 #There will be many instances where this is helpful
 #We may be adding new lines of code
 #We might alter other things as well, such as deleting parts of line 
+#Lets make a change here to work with git diff
