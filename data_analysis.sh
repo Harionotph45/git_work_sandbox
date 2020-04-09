6c2af2b1a748f5049d6a13b89af886285cef1d60
MadMolecularMan <omnitrophic@gmail.com>
2020-04-09 14:12:49 -0400

    new updated method

--- data_analysis.sh
+++ data_analysis.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-#This will contain my new analytical method!
-#Typing for typing
-#We are using this file to aid in using git for version control
-#There will be many instances where this is helpful
-#We may be adding new lines of code
-#We might alter other things as well, such as deleting parts of line
+#New method V2
 
-#Lets make a change here to work with git diff
-
-#Function 1
-
-#Function 2
-
-#Function 3
-
-#Function 4
+#python script goes here
 
 Rscript myrscript.R
